58a8aa881223e957bcbcd7538b2f7c07cd2826eb
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-14 17:37:32 -0300

    feat: configuring dbContext

---
+++ src/IWantApp/Infra/Data/ApplicationDBContext.cs
@@ -0,0 +1,28 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(255);
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Name).IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
