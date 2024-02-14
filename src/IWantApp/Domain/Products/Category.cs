0fcc4229964a79fffc2be576bd134fee9af257f2
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-14 17:14:26 -0300

    feat: domain product

---
+++ src/IWantApp/Domain/Products/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    DateTime EditedOn { get; set; }
+}
