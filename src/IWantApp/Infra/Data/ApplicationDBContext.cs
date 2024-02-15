46639727056dc856d9658dce643f0d29f5f60876
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-15 08:17:38 -0300

    feat: add identity migrations

--- src/IWantApp/Infra/Data/ApplicationDBContext.cs
+++ src/IWantApp/Infra/Data/ApplicationDBContext.cs
@@ -8,12 +8,14 @@ namespace IWantApp.Infra.Data;
 
 public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Ignore<Notification>();
 
         modelBuilder.Entity<Product>()
