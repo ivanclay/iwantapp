43c8ac805dbe228de58dddf2867401df2e5daf2c
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-15 08:05:13 -0300

    feat: add identity entityframeworkcore

--- src/IWantApp/Infra/Data/ApplicationDBContext.cs
+++ src/IWantApp/Infra/Data/ApplicationDBContext.cs
@@ -1,10 +1,12 @@
 ﻿using Flunt.Notifications;
 using IWantApp.Domain.Products;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IWantApp.Infra.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
     public DbSet<Product> Products { get; set; }
