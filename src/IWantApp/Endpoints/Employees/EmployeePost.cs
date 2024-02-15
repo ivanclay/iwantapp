85f6aad07a8596616a1e30fb0cb9b258010a4089
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-15 09:36:56 -0300

    feat: add and refactoring aspnet claims

---
+++ src/IWantApp/Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager) 
+    {
+        var user = new IdentityUser {  UserName = employeeRequest.email, Email = employeeRequest.email };
+        var result = userManager.CreateAsync(user, employeeRequest.password).Result;
+
+        if(!result.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+
+        var userClaims = new List<Claim> 
+        {
+            new Claim("EmployeeCode", employeeRequest.employeeCode),
+            new Claim("Name", employeeRequest.name)
+        };
+
+        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
+
+        if(!claimResult.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+
+        return Results.Created($"/employees/{user.Id}", user.Id);
+    }
+}
--- src/IWantApp/Program.cs
+++ src/IWantApp/Program.cs
@@ -1,10 +1,20 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Endpoints.Employees;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServerConnection"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => 
+{
+    //this define security level
+    //if not use options definitions level is high
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 3;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +36,8 @@ app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle)
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
 app.MapMethods(CategoryDelete.Template, CategoryDelete.Methods, CategoryDelete.Handle);
+
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 
 app.Run();
 
