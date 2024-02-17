5b7fad16d7f7e8bcf723fbb533ff077dc7d95a33
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-17 15:44:33 -0300

    feat: add admin user id to created employee

--- src/IWantApp/Endpoints/Employees/EmployeePost.cs
+++ src/IWantApp/Endpoints/Employees/EmployeePost.cs
@@ -9,25 +9,28 @@ public class EmployeePost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager) 
+    public static IResult Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager) 
     {
-        var user = new IdentityUser {  UserName = employeeRequest.email, Email = employeeRequest.email };
-        var result = userManager.CreateAsync(user, employeeRequest.password).Result;
+        var userAdminId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value; 
+
+        var newUser = new IdentityUser {  UserName = employeeRequest.email, Email = employeeRequest.email };
+        var result = userManager.CreateAsync(newUser, employeeRequest.password).Result;
 
         if(!result.Succeeded)
             return Results.ValidationProblem(result.Errors.ConvertToProblemDetail());
 
-        var userClaims = new List<Claim> 
+        var userClaims = new List<Claim>
         {
             new Claim("EmployeeCode", employeeRequest.employeeCode),
-            new Claim("Name", employeeRequest.name)
+            new Claim("Name", employeeRequest.name),
+            new Claim("CreatedBy", userAdminId)
         };
 
-        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
+        var claimResult = userManager.AddClaimsAsync(newUser, userClaims).Result;
 
         if(!claimResult.Succeeded)
             return Results.BadRequest(result.Errors.First());
 
-        return Results.Created($"/employees/{user.Id}", user.Id);
+        return Results.Created($"/employees/{newUser.Id}", newUser.Id);
     }
 }
