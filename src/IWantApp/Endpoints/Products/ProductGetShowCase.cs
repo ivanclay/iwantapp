8cef6786b225d2be438c86e8a4f0eed88757e8ce
Ivan Clay <ivan.clay.moura@gmail.com>
2024-02-18 10:13:14 -0300

    feat: showcase pagination

--- src/IWantApp/Endpoints/Products/ProductGetShowCase.cs
+++ src/IWantApp/Endpoints/Products/ProductGetShowCase.cs
@@ -12,13 +12,27 @@ public class ProductGetShowCase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context) 
+    public static async Task<IResult> Action(int? page, int? rows, string? orderby, ApplicationDbContext context) 
     {
-        var products = context.Products
+        if (page == null) page = 1;
+        if (rows == null) rows = 10;
+        if (string.IsNullOrEmpty(orderby)) orderby = "name";
+
+        var queryBase = context.Products
             .Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name)
-            .ToList();
+            .Where(p => p.HasStock && p.Category.Active);
+
+        var filterQuery = queryBase;
+
+        if (orderby.Equals("name"))
+            filterQuery = filterQuery.OrderBy(p => p.Name);
+        else
+            filterQuery = filterQuery.OrderBy(p => p.Price);
+        
+        filterQuery = filterQuery.Skip((page.Value - 1) * rows.Value).Take(rows.Value);
+
+        var products = filterQuery.ToList();
+
         var results = products
             .Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
         return Results.Ok(results);
